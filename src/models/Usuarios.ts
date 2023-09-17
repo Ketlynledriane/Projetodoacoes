e2d429a14e4c01504119b6c88605a200d92d4d9a
ketlynledriane <ketlyn-ledriane@hotmail.com>
2023-09-17 17:02:10 -0300

    Criado coluna usuarios

---
+++ src/models/Usuarios.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('usuarios')
+export class Usuarios extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Column()
+    public nome: string;
+
+    @Column()
+    public email: string;
+
+    @Column()
+    public senha: string;
+    
+}
