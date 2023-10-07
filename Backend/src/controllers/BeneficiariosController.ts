f720019569f2a27538b3bd90a56b00cfffa32e0f
AmandaSchererMeirelles <mandy.floripa2015@hotmail.com>
2023-10-07 09:52:02 -0300

    BeneficiariosController

--- Backend/src/controllers/BeneficiariosController.ts
+++ Backend/src/controllers/BeneficiariosController.ts
@@ -33,31 +33,31 @@ export class BeneficiariosControllers {
     async delete (req: Request, res: Response): Promise<Response> {
       let id = Number(req.params.id);
     
-      let beneficiairo: Beneficiarios|null = await Beneficiarios.findOneBy({ id });
-      if (! beneficiairo) {
-        return res.status(422).json({ error: 'Usuario não encontrado!' });
+      let beneficiario: Beneficiarios|null = await Beneficiarios.findOneBy({ id });
+      if (! beneficiario) {
+        return res.status(422).json({ error: 'Beneficiario não encontrado!' });
       }
     
-      beneficiairo.remove();
+      beneficiario.remove();
     
       return res.status(200).json();
     }
 
     async find (req: Request, res: Response): Promise<Response> {
-      let beneficiairo: Beneficiarios= res.locals.beneficiairo;
-      return res.status(200).json(beneficiairo);
+      let beneficiario: Beneficiarios= res.locals.beneficiario;
+      return res.status(200).json(beneficiario);
     
     }
 
     async update (req: Request, res: Response): Promise<Response> {
         let body = req.body;
-        let beneficiairo: Beneficiarios = res.locals.beneficiairo;
+        let beneficiario: Beneficiarios = res.locals.beneficiario;
     
-        beneficiairo.nome = body.nome,
-        beneficiairo.cpf = body.cpf,
-        beneficiairo.id_cidade = body.id_cidade,
-        await beneficiairo.save();
+        beneficiario.nome = body.nome,
+        beneficiario.cpf = body.cpf,
+        beneficiario.id_cidade = body.id_cidade,
+        await beneficiario.save();
     
-        return res.status(200).json(beneficiairo);
+        return res.status(200).json(beneficiario);
     }
 }
