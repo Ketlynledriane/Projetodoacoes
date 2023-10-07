2f6c6cb28bd6a323c35a30638969ea6af4af1dbe
AmandaSchererMeirelles <mandy.floripa2015@hotmail.com>
2023-10-07 09:16:22 -0300

    cidadesController delete

--- Backend/src/controllers/CidadesController.ts
+++ Backend/src/controllers/CidadesController.ts
@@ -22,7 +22,7 @@ export class CidadesController {
         return res.status(200).json(cidade);
     }
     async delete (req: Request, res: Response): Promise<Response> {
-        let cidade: Cidades= res.locals.categoria;
+        let cidade: Cidades= res.locals.cidade;
     
         cidade.remove();
         
